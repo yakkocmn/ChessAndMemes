0f2a9fc278267749ab0d6c62648b7b76a0f98f5b
Chris Newsome <legoclonetrooperdude@gmail.com>
2015-10-28 10:58:29 -0700

    new files and created cells/board

--- chess.js
+++ chess.js
@@ -1,10 +1,48 @@
 
-var canvas = document.getElementbyId("gameArea");
+var canvas = document.getElementById("gameArea");
 canvasGA.addEventListener( 'click', processUserInput);
 var contextGA = canvasGA.getContext("2d");
 
+var cells = new Array(8);
+for(var i = 0; i < 8; i++)
+  cells[i] = new Array(8);
 
+function setupBoard() {
+  for(var r = 0; r < 8; r++) {
+    for(var c = 0; c < 8; c++) {
+      cells[r][c] = new Cell();
+      //alternate square colors
+      if((r+c)%2 == 0)
+        cells[r][c].backgroundColor = "black";
+    }
+  }
+  cells[1][0].piece = new Pawn("white");
+}
 
+function drawBoard() {
+  for(var r = 0; r < 8; r++) {
+    for(var c = 0; c < 8; c++) {
+        
+    }
+  }
+}
+
+function Cell() {
+  this.backgroundColor = "white";
+  this.piece = new EmptyPiece();
+}
+
+function EmptyPiece() {
+  this.image = new Image();
+  this.image.src = "piece-images/emptyPiece.png";
+  this.playerColor = "none";
+}
+
+function Pawn(color) {
+  this.playerColor = color;
+  this.image = new Image();
+  this.image.src = "piece-images/pawn-" + this.playerColor + ".png";
+}
 
 function processUserInput(event) {
   console.log("stuff was clicked: " + (event.x - canvasGA.offsetLeft));
