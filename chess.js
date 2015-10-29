3c4515f7e7b4dfac26e12a8cbdad4b5a9bc130f5
Chris Newsome <legoclonetrooperdude@gmail.com>
2015-10-29 14:23:42 -0700

    finished basic design and started adding pieces to board

--- chess.js
+++ chess.js
@@ -1,11 +1,16 @@
 
-var canvas = document.getElementById("gameArea");
-canvasGA.addEventListener( 'click', processUserInput);
+var canvasGA = document.getElementById("gameArea");
+canvasGA.addEventListener('click', processUserInput);
 var contextGA = canvasGA.getContext("2d");
+
+var cellSize = canvasGA.width/8;
 
 var cells = new Array(8);
 for(var i = 0; i < 8; i++)
   cells[i] = new Array(8);
+
+setupBoard();
+setTimeout(drawBoard, 100);
 
 function setupBoard() {
   for(var r = 0; r < 8; r++) {
@@ -13,22 +18,31 @@ function setupBoard() {
       cells[r][c] = new Cell();
       //alternate square colors
       if((r+c)%2 == 0)
-        cells[r][c].backgroundColor = "black";
+        cells[r][c].backgroundColor = "#b58863";
     }
   }
-  cells[1][0].piece = new Pawn("white");
+}
+
+for(var blah = 0; blah < 8; blah++){
+  cells[1][blah].piece = new Pawn("black");
+  cells[6][blah].piece = new Pawn("white");
+  cells[0][0].piece = new Rook("black");
+  cells[0][7].piece = new Rook("black");
+  cells[0][1]
 }
 
 function drawBoard() {
   for(var r = 0; r < 8; r++) {
     for(var c = 0; c < 8; c++) {
-        
+        contextGA.fillStyle = cells[r][c].backgroundColor;
+        contextGA.fillRect(c*cellSize, r*cellSize, cellSize, cellSize);
+        contextGA.drawImage(cells[r][c].piece.image, c*cellSize, r*cellSize, cellSize, cellSize);
     }
   }
 }
 
 function Cell() {
-  this.backgroundColor = "white";
+  this.backgroundColor = "#f0d9b5";
   this.piece = new EmptyPiece();
 }
 
@@ -44,6 +58,19 @@ function Pawn(color) {
   this.image.src = "piece-images/pawn-" + this.playerColor + ".png";
 }
 
+function Rook(color) {
+  this.playerColor = color;
+  this.image = new Image();
+  this.image.src = "piece-images/rook-" + this.playerColor + ".png";
+}
+
+function Knight(color) {
+  this.playerColor = color;
+  this.image = new Image();
+  this.image.src = "piece-images/pawn-" + this.playerColor + ".png";
+}
+
 function processUserInput(event) {
   console.log("stuff was clicked: " + (event.x - canvasGA.offsetLeft));
+  drawBoard();
 }
